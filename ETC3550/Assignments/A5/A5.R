f5e19e572b580b23c630c103826a68863f9cfdf9
Chelaka Paranahewa <chelaka.paranahewa@gmail.com>
2022-04-14 15:43:30 +1000

    update function

--- ETC3550/Assignments/A5/A5.R
+++ ETC3550/Assignments/A5/A5.R
@@ -2,50 +2,39 @@ library(fpp3, warn.conflicts = FALSE)
 library(tidyverse, warn.conflicts = FALSE)
 
 ses_func <- function(y, forecast = 1, alpha_input = 0) {
-    smooth_function <- function(y, alpha, level) {
+    smooth_function <- function(y, alpha, level, sse_mode = FALSE) {
         n <- length(y)
         levels <- numeric(n + 1)
-        levels[1] <- level
+
+        levels[1] <- alpha * y[1] + (1 - alpha) * level
+
         for (t in 1:n) {
             levels[t + 1] <- alpha * y[t] + (1 - alpha) * levels[t]
         }
+        # Returns Sum of Squared Errors
+        if (sse_mode) {
+            return(sum((y - levels[-(n + 1)])^2))
+        }
+
+        # Returns yhat_(t+h)
         return(levels[n + 1])
     }
 
-    # Finds the best alpha value for given dataset
-    alpha_cal <- function(y, alpha_g, level) {
-        # adjust incrementor for more fined alpha value at the cost of time
-        alpha_incrementor <- 10^(-5)
-        sse_best <- Inf
+    sse <- function(par, y) {
+        return(smooth_function(y, par[1], par[2], TRUE))
+    }
 
-        # Do while loop
-        repeat{
-            n <- length(y)
-            levels <- numeric(n + 1)
-            levels[1] <- level
-            for (t in 1:n) {
-                levels[t + 1] <- alpha_g * y[t] + (1 - alpha_g) * levels[t]
-            }
-            yhat <- levels[-(n + 1)]
-            sse <- sum((y - yhat)^2)
-            if (sse > sse_best) {
-                break
-            }
-            alpha_g <- alpha_g + alpha_incrementor
-            sse_best <- sse
-        }
-        return(alpha_g)
-    }
-    if (forecast == 1) {
-        return(smooth_function(y, alpha_cal(y, alpha_input, y[1]), y[1]))
-    }
-    while (forecast > 1) {
-        print(forecast)
-        y <- append(
-            y,
-            smooth_function(y, alpha_cal(y, alpha_input, y[1]), y[1])
-        )
-        forecast <- forecast - 1
-    }
-    return(y[length(y)])
+    best_val <- optim(
+        par = c(
+            alpha = alpha_input,
+            level = y[1]
+        ),
+        fn = sse,
+        y = y
+    )
+
+    # Forecast you return the same value for any value of h
+    # Since the formula for SES shows that h has no influence
+    # on the final output
+    return(smooth_function(y, best_val$par[1], best_val$par[2]))
 }
